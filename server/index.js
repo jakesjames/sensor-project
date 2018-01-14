8b37940048b1c68d7369954ead9c574711264089
jakesjames <jacobstreatfeildjames.com>
2018-01-14 16:51:10 +0000

    updated index.js

---
+++ server/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express()
+app.get('/temperature', function(req, res) {res.send('24 °C');
+});
+app.get('/humidity', function(req, res) {res.send('48%');
+});
