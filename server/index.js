b153dbb202fe43d809e14161770bf07e6768d876
jakesjames <jacobstreatfeildjames@gmail.com>
2018-01-27 20:43:13 +0000

    pp85

--- server/index.js
+++ server/index.js
@@ -4,6 +4,15 @@ const app = express();
 const getCachedSensorReadings = require('./get-cached-sensor-readings')
 
 /*
+Here, we are introduced to express middleware.  Middleware is a fancy word to describe a set of actions
+that have to take place before the request handler. 
+
+In the statement below, we use the express.static middleware, and bind it to the /public route
+*/
+
+app.use('/public', express.static(path.join(__dirname, 'public')))
+
+/*
 we now utilize the synchronous methods exported from the 'get-cached-sensor-readings' module
 */
 
@@ -14,10 +23,11 @@ app.get('/temperature', function (req, res) {
 app.get('/humidity', function (req, res) {
 	res.send(getCachedSensorReadings.getHumidity().toFixed(1) + '%');
 })
-
+/*
 app.get('/public', function (req, res) {
 	res.sendFile(path.join(__dirname, 'index.html'))
 })
+*/
 
 app.listen(3000, function(){
 	console.log('Server listening on port 3000');
