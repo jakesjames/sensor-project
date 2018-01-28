450790f4dfa483d8638485599715fb6037c549a5
jakesjames <jacobstreatfeildjames@gmail.com>
2018-01-28 14:58:27 +0000

    pp91

--- server/public/script.js
+++ server/public/script.js
@@ -1,8 +1,26 @@
-fetch('/temperature')
-.then(results => {
-	return results.text()
-})
-.then(text => {
-	const temperatureDisplay = document.getElementById('temperature-display');
-	temperatureDisplay.innerHTML = text
-})
+const fetchTemperature = () => {
+	fetch('/temperature')
+	.then(results => {
+		return results.text()
+	})
+	.then(text => {
+		const temperatureDisplay = document.getElementById('temperature-display')
+		temperatureDisplay.innerHTML = text
+	})
+}
+
+const fetchHumidity = () => {
+	fetch('/humidity')
+	.then(results => {
+		return results.text()
+	})
+	.then(text => {
+		const humidityDisplay = document.getElementById('humidity-display')
+		humidityDisplay.innerHTML = text
+		})
+}
+
+setInterval(() => {
+	fetchTemperature()
+	fetchHumidity()
+}, 2000)
