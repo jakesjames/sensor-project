8645b1b46fe5e027f6267968cc194e6f621de574
jakesjames <jacobstreatfeildjames@gmail.com>
2018-01-31 20:10:29 +0000

    pp 104

--- server/public/script.js
+++ server/public/script.js
@@ -24,3 +24,35 @@ setInterval(() => {
 	fetchTemperature()
 	fetchHumidity()
 }, 2000)
+
+/*
+*Get the context of the temperature canvas element
+*/
+
+const temperatureCanvasCtx = document.getElementById('temperature-chart').getContext('2d')
+
+/*
+Create a new chart on the context we just instantiated
+*/
+
+const temperatureChart = new Chart(temperatureCanvasCtx, 
+{
+	/*
+	We're going to show the ongoing temperature as a line chart
+	*/
+	type: 'line',
+	data: {
+		labels: ['10:30', '10:31', '10:32', '10:32'],
+		datasets: [{
+			data: [12, 19, 23, 17],
+			backgroundColor: 'rgba(255, 205, 210, 0.5)'
+		}]
+	},
+	options: {
+		legend: {
+			display: false
+		},
+		responsive: true,
+		maintainAspectRatio: false
+	}
+})
