de993d33372cff761d15f5a98b7c1c1cfabf4ee7
jakesjames <jacobstreatfeildjames@gmail.com>
2018-02-03 15:39:42 +0000

    pp116

--- server/public/script.js
+++ server/public/script.js
@@ -1,10 +1,82 @@
+const temperatureCanvasCtx = document.getElementById('temperature-chart').getContext('2d')
+
+const temperatureChartConfig = {
+	type: 'line',
+	data: {
+		labels: [],
+		datasets: [{
+			data: [],
+			bacgroundColor: 'rgba(255, 205, 210, 0.5)'
+		}]
+	},
+	options: {
+		legend: {
+			display: false
+		},
+		responsive: true,
+		maintainAspectRatio: false,
+		scales: {
+			yAxes: [{
+				ticks: {
+					suggestedMin: 10,
+					suggestedMax: 40
+				}
+			}]
+		}
+	}
+}
+const temperatureChart = new Chart(temperatureCanvasCtx, temperatureChartConfig)
+
+const humidityCanvasCtx = document.getElementById('humidity-chart').getContext('2d')
+
+const humidityChartConfig = {
+	type: 'line',
+	data: {
+		labels: [],
+		datasets: [{
+			data: [],
+			bacgroundColor: 'rgba(197, 202, 233, 0.5)'
+		}]
+	},
+	options: {
+		legend: {
+			display: false
+		},
+		responsive: true,
+		maintainAspectRatio: false,
+		scales: {
+			yAxes: [{
+				ticks: {
+					suggestedMin: 30,
+					suggestedMax: 90
+				}
+			}]
+		}
+	}
+}
+const humidityChart = new Chart (humidityCanvasCtx, humidityChartConfig)
+
+const pushData = (arr, value, maxLen) => {
+	arr.push(value)
+	if(arr.length > maxLen) {
+		arr.shift()
+	}
+}
+
+const humidityDisplay = document.getElementById('humidity-display')
+const temperatureDisplay = document.getElementById('temperature-display')
+
 const fetchTemperature = () => {
 	fetch('/temperature')
 	.then(results => {
 		return results.json()
 	})
 	.then(data => {
-		const temperatureDisplay = document.getElementById('temperature-display')
+		const now = new Date()
+		const timeNow = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds()
+		pushData(temperatureChartConfig.data.labels, timeNow, 10)
+		pushData(temperatureChartConfig.data.datasets[0].data, data.value, 10)
+		temperatureChart.update()
 		temperatureDisplay.innerHTML = '<strong>' + data.value + '</strong>'
 	})
 }
@@ -15,44 +87,16 @@ const fetchHumidity = () => {
 		return results.json()
 	})
 	.then(data => {
-		const humidityDisplay = document.getElementById('humidity-display')
+		const now = new Date()
+		const timeNow = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds()
+		pushData(humidityChartConfig.data.labels, timeNow, 10)
+		pushData(humidityChartConfig.data.datasets[0].data, data.value, 10)
+		humidityChart.update()
 		humidityDisplay.innerHTML = '<strong>' + data.value + '</strong>'
-		})
+	})
 }
 
 setInterval(() => {
 	fetchTemperature()
 	fetchHumidity()
 }, 2000)
-
-/*
-*Get the context of the temperature canvas element
-*/
-
-const temperatureCanvasCtx = document.getElementById('temperature-chart').getContext('2d')
-
-/*
-Create a new chart on the context we just instantiated
-*/
-
-const temperatureChart = new Chart(temperatureCanvasCtx, 
-{
-	/*
-	We're going to show the ongoing temperature as a line chart
-	*/
-	type: 'line',
-	data: {
-		labels: ['10:30', '10:31', '10:32', '10:32'],
-		datasets: [{
-			data: [12, 19, 23, 17],
-			backgroundColor: 'rgba(255, 205, 210, 0.5)'
-		}]
-	},
-	options: {
-		legend: {
-			display: false
-		},
-		responsive: true,
-		maintainAspectRatio: false
-	}
-})
