55cd1c2d4fb3647fbe69cc52e8eb7e7e2d6d3fd2
jakesjames <jacobstreatfeildjames@gmail.com>
2018-02-10 08:51:21 +0000

    pp142 corrections

--- server/get-cached-sensor-readings.js
+++ server/get-cached-sensor-readings.js
@@ -1,23 +1,11 @@
 const getSensorReadings = require('./get-sensor-readings')
 
-/*
-Import the database module that we created earlier
-*/
-
 const databaseOperations = require('./database-operations')
-
-/*
-Instantiate the cache. In this case its a simple variable stored in the local memory.
-*/
 
 const cache = {
 	temperature: 0,
 	humidity: 0
 }
-
-/*
-Run a function to get the sensor readings every 2 seconds (the same sampling rate as our sensor)
-*/
 
 setInterval(() => {
 	getSensorReadings((err, temperature, humidity) => {
@@ -35,8 +23,5 @@ setInterval(() => {
 	})
 }, 2000)
 
-/*
-The functions that we expose only return the cached values, and don't make a call to the sensor interface every time.
-*/
 module.exports.getTemperature = () => cache.temperature
 module.exports.getHumidity = () => cache.humidity
